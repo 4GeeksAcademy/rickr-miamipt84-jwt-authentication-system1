bac8499862a56d481ed3d0edb9d20ba90a81a59b
Rick Rodriguez <rickrod6502@gmail.com>
2025-11-06 01:22:32 +0000

    added commands to auto generate users and invoices

--- src/front/pages/Private.jsx
+++ src/front/pages/Private.jsx
@@ -1,9 +1,26 @@
+import useGlobalReducer from "../hooks/useGlobalReducer"
 
 export const Private = () => {
+    const { store, dispatch } = useGlobalReducer();
+
+    // add a useEffect to fetch private user information
+    // when the user is successfully logged in
+    // useEffect()
 
     return (
-        <div className="text-center mt-5">
-            Hello from the Private page!
-        </div>
+        <>
+            {
+                (store.isLoggedIn)
+                ?
+                <div className="text-center mt-5">
+                    Hello from the Private page!
+                </div>
+                :
+                <div className="text-center mt-5">
+                    You are not logged in. Please log in!
+                </div>
+            }
+        </>
+        
     );
 };
